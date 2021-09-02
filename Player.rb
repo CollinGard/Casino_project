c84205a881f0581c891567420f61562f8195696b
DavMSmith <davonda.smith@gmail.com>
2021-09-02 12:43:48 -0400

    Coded highlow game.Suggested class name correction to player file.

--- Player.rb
+++ Player.rb
@@ -1,4 +1,6 @@
 class Player
+  #Davonda capitalized player when prompted by error message in terminal
+  # Error stated " class/module name must be CONSTANT (SyntaxError)"
   attr_accessor :name, :wallet
   def initialize
     puts "Please type in your name"
